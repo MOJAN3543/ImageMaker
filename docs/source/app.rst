82588f2723d5c8098293972047916af28501eb09
MOJAN3543 <sjs732@naver.com>
2026-04-10 10:30:53 +0900

    Docs: Sphinx 문서 추가

---
+++ docs/source/app.rst
@@ -0,0 +1,29 @@
+app package
+===========
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   app.util
+
+Submodules
+----------
+
+app.app module
+--------------
+
+.. automodule:: app.app
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: app
+   :members:
+   :show-inheritance:
+   :undoc-members:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Allow Sphinx to import the project package from the repository root.
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Mini Project"
+copyright = '2026, junseong sim'
+author = 'junseong sim'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
